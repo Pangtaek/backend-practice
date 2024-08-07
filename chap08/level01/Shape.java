9ef0b2c121cad4b1e12a024c07f2550df92c777b
Pangtaek <zombie3632@gmail.com>
2024-08-07 11:34:34 +0900

    chap08-polymorphism

---
+++ chap08/level01/Shape.java
@@ -0,0 +1,9 @@
+package com.pangtaek.practice.chap08.level01;
+
+public abstract class Shape {
+    /* 넓이 계산 메소드 */
+    abstract double calculateArea();
+    /* 둘레 계산 메소드 */
+    abstract double calculatePerimeter();
+}
+
