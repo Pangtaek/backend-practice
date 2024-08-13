96297d786e33fa565e1c684dad2382a54a190090
Pangtaek <zombie3632@gmail.com>
2024-08-13 15:19:13 +0900

    chap13-collection-Application03 ArrayList -> Queue

--- chap13/level01/Application03.java
+++ chap13/level01/Application03.java
@@ -3,8 +3,8 @@ package com.pangtaek.practice.chap13.level01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class Application03 {
@@ -24,27 +24,30 @@ public class Application03 {
 //    대기 고객이 없습니다.
 //    대기 고객 이름 입력 (다음 고객으로 넘어가려면 'next', 종료하려면 'exit'): exit
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-        int index = 0;
+        Queue<String> queue = new LinkedList<String>();
 
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            while(true){
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
                 System.out.print("대기 고객 이름 입력 (다음 고객으로 넘어가려면 'next', 종료하려면 'exit'): ");
                 String input = br.readLine();
-                if(input.equals("exit")){
+                if (input.equals("exit")) {
                     break;
-                } else if(input.equals("next")){
-                    try{
-                        System.out.println(list.get(index++) + " 고객님 차례입니다.");
-                    } catch(IndexOutOfBoundsException e){
+                } else if (input.equals("next")) {
+                    try {
+                        if (!queue.isEmpty()) {
+                            System.out.println(queue.poll() + " 고객님 차례입니다.");
+                        } else {
+                            System.out.println("대기 고객이 없습니다.");
+                        }
+                    } catch (IndexOutOfBoundsException e) {
                         System.out.println("대기 고객이 없습니다.");
                     }
-                } else{
-                    list.add(input);
+                } else {
+                    queue.add(input);
                     System.out.println(input + " 고객님 대기 등록 되었습니다.");
                 }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
