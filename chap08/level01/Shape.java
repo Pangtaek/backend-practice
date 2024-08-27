686e37f43f96b814776400ca0ec0237db8b2b6e3
Pangtaek <zombie3632@gmail.com>
2024-08-27 17:46:10 +0900

    파일 구조 변경

--- chap08/level01/Shape.java
+++
@@ -1,9 +0,0 @@
-package com.pangtaek.practice.chap08.level01;
-
-public abstract class Shape {
-    /* 넓이 계산 메소드 */
-    abstract double calculateArea();
-    /* 둘레 계산 메소드 */
-    abstract double calculatePerimeter();
-}
-
